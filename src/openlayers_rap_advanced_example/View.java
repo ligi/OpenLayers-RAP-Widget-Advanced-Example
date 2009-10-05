f22724a34ad9c05555da2f21d2717b436d944d29
ligi <ligi@42dd07d8-1ee8-11de-aac7-e55ba83f00cf>
2009-10-05 11:32:52 +0000

    bugfixes / create shell on click / add boxes
    git-svn-id: https://polymap.org/repos/polymap3/trunk/org.polymap.rap.widget.openlayers/examples/advanced@1319 42dd07d8-1ee8-11de-aac7-e55ba83f00cf

--- src/openlayers_rap_advanced_example/View.java
+++ src/openlayers_rap_advanced_example/View.java
@@ -39,6 +39,14 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 import org.polymap.rap.widget.openlayers.OpenLayers;
+import org.polymap.rap.widget.openlayers.base_types.Bounds;
+import org.polymap.rap.widget.openlayers.controls.KeyboardDefaultsControl;
+import org.polymap.rap.widget.openlayers.controls.LayerSwitcherControl;
+import org.polymap.rap.widget.openlayers.controls.MouseDefaultsControl;
+import org.polymap.rap.widget.openlayers.controls.PanZoomBarControl;
+import org.polymap.rap.widget.openlayers.controls.SelectFeatureControl;
+import org.polymap.rap.widget.openlayers.features.VectorFeature;
+import org.polymap.rap.widget.openlayers.layers.VectorLayer;
 import org.polymap.rap.widget.openlayers.layers.WMSLayer;
 
 /**
@@ -55,18 +63,27 @@ public class View
 
 	public static final String ID = "OpenLayers_RAP_Advanced_Example.view";
 
-	private Shell add_wms_shell,add_control_shell,edit_center_shell;
+	private Shell add_wms_shell,add_control_shell,edit_center_shell,create_boxes_shell;
 	
 	private Display display;
-	private Button open_add_wms_shell_btn,open_set_center_btn,open_add_control_btn;
+	private Button open_add_wms_shell_btn,open_set_center_btn,open_add_control_btn,open_create_boxes_btn;
 	private OpenLayers openlayers;
 	private Text wms_add_layers,wms_add_url,wms_add_name,center_lon_field,center_lat_field,zoom_field,add_control_field;
-	private Button add_wms_btn,add_control_btn,set_center_btn,load_wms_example ;
+	private Text boxes_name_field,boxes_x_count_field,boxes_y_count_field;
+	private Button add_wms_btn,add_control_btn,set_center_btn,load_wms_example,create_boxes_btn ;
+	
+	private Boolean center_set=false;
 	
 	private Font boldFont;
 	
-	public void createAddWMSShell() {
+	public void showAddWMSShell() {
+		
+		if (add_wms_shell!=null)
+				add_wms_shell.close();
+		
 		add_wms_shell=new Shell(display);
+		
+			
 		FillLayout toolbar_layout=new FillLayout();
 		toolbar_layout.type=SWT.HORIZONTAL;
 		add_wms_shell.setLayout( toolbar_layout );
@@ -98,10 +115,15 @@ public class View
 		add_wms_btn.setText("Add");
 		add_wms_btn.addMouseListener(this);	
 		
+		add_wms_shell.open();
+		add_wms_shell.setFocus();
 	}
 
-	public void createEditCenterShell() {
+	public void showEditCenterShell() {
 	
+		if (edit_center_shell!=null)
+			edit_center_shell.close();
+		
 		edit_center_shell=new Shell(display);
 		FillLayout toolbar_layout=new FillLayout();
 		toolbar_layout.type=SWT.HORIZONTAL;
@@ -129,10 +151,56 @@ public class View
 		set_center_btn=new Button(edit_center_shell,SWT.NONE | SWT.BORDER);
 		set_center_btn.setText("OK");
 		set_center_btn.addMouseListener(this);	
-		
+	
+		edit_center_shell.open();
+		edit_center_shell.setFocus();
 	}
 
-	public void createAddControlShell() {
+	
+	public void showCreateBoxesShell() {
+		
+		if (create_boxes_shell!=null)
+			create_boxes_shell.close();
+		
+		create_boxes_shell=new Shell(display);
+		FillLayout toolbar_layout=new FillLayout();
+		toolbar_layout.type=SWT.HORIZONTAL;
+		
+		create_boxes_shell.setLayout( toolbar_layout );
+		create_boxes_shell.setText( "Create Boxes" );
+		create_boxes_shell.setSize(400,50);
+	
+		Label l=new Label(create_boxes_shell,SWT.NONE);
+		l.setFont(boldFont);
+		l.setText("Name:");
+		
+		boxes_name_field=new Text(create_boxes_shell,SWT.NONE | SWT.BORDER);
+		//new Label(add_wms_shell,SWT.NONE).setText("URL:");
+		l=new Label(create_boxes_shell,SWT.NONE);
+		l.setFont(boldFont);
+		l.setText("X-Count");
+		boxes_x_count_field=new Text(create_boxes_shell,SWT.NONE | SWT.BORDER);
+		l=new Label(create_boxes_shell,SWT.NONE);
+		l.setFont(boldFont);
+		l.setText("Y-Count:");
+
+		//new Label(add_wms_shell,SWT.NONE).setText("Layers:");
+		boxes_y_count_field=new Text(create_boxes_shell,SWT.NONE | SWT.BORDER);
+		
+		create_boxes_btn=new Button(create_boxes_shell,SWT.NONE | SWT.BORDER);
+		create_boxes_btn.setText("OK");
+		create_boxes_btn.addMouseListener(this);	
+	
+		create_boxes_shell.open();
+		create_boxes_shell.setFocus();
+	}
+
+	
+	public void showAddControlShell() {
+		
+		if (add_control_shell!=null)
+			add_control_shell.close();
+		
 		add_control_shell=new Shell(display);
 		FillLayout toolbar_layout=new FillLayout();
 		toolbar_layout.type=SWT.VERTICAL;
@@ -145,6 +213,9 @@ public class View
 		add_control_btn=new Button(add_control_shell,SWT.NONE | SWT.BORDER);
 		add_control_btn.setText("OK");
 		add_control_btn.addMouseListener(this);	
+	
+		add_control_shell.open();
+		add_control_shell.setFocus();
 	}
 
 	public void createPartControl(Composite parent) {
@@ -152,10 +223,6 @@ public class View
 		
 		// setup bold font
 		boldFont = JFaceResources.getFontRegistry().getBold(JFaceResources.DEFAULT_FONT);    
-		
-		createAddWMSShell();
-		createEditCenterShell();
-		createAddControlShell();
 		
 		Composite top = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
@@ -182,12 +249,25 @@ public class View
 		open_set_center_btn.setText("set Center");
 		open_set_center_btn.addMouseListener(this);
 		
+		
+		open_create_boxes_btn=new Button(banner,SWT.PUSH);
+		open_create_boxes_btn.setText("add Boxes");
+		open_create_boxes_btn.addMouseListener(this);
+		
 		/*open_add_control_btn= new Button(banner,SWT.PUSH);
 		open_add_control_btn.setText("add Control");
 		open_add_control_btn.addMouseListener(this);
 		*/
 		openlayers=new OpenLayers(top,SWT.MULTI | SWT.WRAP);
 		openlayers.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		
+		// add some controls
+		openlayers.addControl(new LayerSwitcherControl());
+		openlayers.addControl(new MouseDefaultsControl());
+		openlayers.addControl(new KeyboardDefaultsControl());
+		openlayers.addControl(new PanZoomBarControl());
+		
 	}
 
 	public void setFocus() {
@@ -201,21 +281,23 @@ public class View
 	@Override
 	public void mouseDown(MouseEvent e) {
 		Object src=e.getSource();
-		if ( src==open_add_wms_shell_btn) {
-			add_wms_shell.open();
-			add_wms_shell.setFocus();
-		}
-		else if (src==open_set_center_btn) {
-			edit_center_shell.open();
-			edit_center_shell.setFocus();
-		}
-		else if (src==open_add_control_btn) {
-			add_control_shell.open();
-			add_control_shell.setFocus();
-		}
+		if ( src==open_add_wms_shell_btn) 
+			showAddWMSShell();
+		else if (src==open_set_center_btn) 
+			showEditCenterShell();
+		else if (src==open_add_control_btn)
+			showAddWMSShell();
+		else if (src==open_create_boxes_btn)
+			showCreateBoxesShell();
 		else if (src== add_wms_btn) {
 			WMSLayer wms_layer=new WMSLayer( wms_add_name.getText(), wms_add_url.getText(), wms_add_layers.getText());
 			openlayers.addLayer(wms_layer);
+			
+			if (!center_set) {
+				openlayers.setCenter(0,0);
+				openlayers.zoomTo(1);
+			}
+			
 		}
 		else if (src== load_wms_example) {
 			wms_add_name.setText("polymap");
@@ -225,7 +307,32 @@ public class View
 		else if ( src==set_center_btn) {
 			openlayers.setCenter(new Double(center_lon_field.getText()), new Double(center_lat_field.getText()));
 			openlayers.zoomTo(Integer.parseInt(zoom_field.getText()));
+			center_set=true;
 		}
+		else if (src==create_boxes_btn)		{
+			VectorLayer multibox_layer=new VectorLayer(  boxes_name_field.getText() );
+			openlayers.addLayer(multibox_layer);
+			int x_size= Integer.parseInt(boxes_x_count_field.getText());
+			int y_size= Integer.parseInt(boxes_y_count_field.getText());
+			
+			for ( int x = -(x_size/2) ; x<(-(x_size/2)+x_size); x++ )
+				for ( int y = -(y_size/2) ; y<(-(y_size/2)+y_size) ;y++ )
+					multibox_layer.addFeatures(new VectorFeature(new Bounds(2.0*x, 2.0*y ,2.0*x+1.8 ,2.0*y+1.8)));
+				
+				
+			// setting up the Modify Feature Control
+			SelectFeatureControl sfc=new SelectFeatureControl(multibox_layer);
+		
+			openlayers.addControl(sfc);
+			sfc.activate();
+			
+			if (!center_set) {
+				openlayers.setCenter(0,0);
+				openlayers.zoomTo(1);
+			}
+			
+		}
+		
 		/*else if ( src == add_control_btn)
 			openlayers.addControl(add_control_field.getText());
 			*/
